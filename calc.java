a27e136a5becfa341cdd3c5be1205c9882302374
patrickjdoherty <L00120294@student.lyit.ie>
2018-11-17 15:16:44 +0000

    simple calc first commit

---
+++ calc.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class calc
+{
+	public static void main(String[] args)
+	{
+		int num1, num2;
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter Num 1: ");
+		num1 = input.nextInt();
+		System.out.println("Enter num 1: ");
+		num2 = input.nextInt();
+
+		System.out.println("Answer " + (num1+num2));
+	}
+}
+
